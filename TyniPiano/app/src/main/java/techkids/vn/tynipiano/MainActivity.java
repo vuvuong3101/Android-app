a823c93c37e19056440cfc6f81ad653441dfbc9e
vuvuong3101 <admin@MacBook-Pro-cua-Admin.local>
2017-05-27 22:29:50 +0700

    HomeWork ss1
    tạo UI phím đàn

---
+++ TyniPiano/app/src/main/java/techkids/vn/tynipiano/MainActivity.java
@@ -0,0 +1,24 @@
+package techkids.vn.tynipiano;
+
+import android.icu.text.LocaleDisplayNames;
+import android.nfc.Tag;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.toString() ;
+    private EditText  etName;
+    private Button btHello;
+    private TextView tvHelloString;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
